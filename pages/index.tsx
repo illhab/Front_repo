26d30a5feeb89c614da7e9f4724cafe47e8e272f
myMac <dgbuzzer@gmail.com>
2023-10-14 13:05:35 +0900

    feature/FRON-11 register page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import {AppLayout} from '@/layouts';
-import {LogIn} from '@/containers';
+import {LogIn, Register} from '@/containers';
 
 export default function Home() {
-  return <LogIn />;
+  //return <LogIn />;
+  return <Register />;
 }
