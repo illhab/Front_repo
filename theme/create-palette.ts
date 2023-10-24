2e981954265c377c7aec2cfbf063ed589906f92d
hyeongjin <kimjin5537@imform.co.kr>
2023-10-24 10:34:37 +0900

    palette contrastText

--- theme/create-palette.ts
+++ theme/create-palette.ts
@@ -2,9 +2,11 @@ export function createPalette() {
   return {
     primary: {
       main: '#00FF7F',
+      contrastText: '#0e1115',
     },
     secondary: {
       main: '#20212D',
+      contrastText: '#fff',
     },
   };
 }
